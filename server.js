77753cec7f2869740087748a551c20468ebf1006
Hetal9Desai <hetal.desai@simformsolutions.com>
2025-05-29 23:26:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,16 +5,28 @@ const PORT = process.env.PORT || 3000;
 
 const data = require("./db.json");
 
-app.use(cors());
+const whitelist = [
+  "https://lucky-arithmetic-f32e7f.netlify.app", 
+  "http://localhost:3000"                          
+];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (!origin || whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
-app.get("/users", (req, res) => {
-  res.json(data.users);
-});
-
-app.get("/products", (req, res) => {
-  res.json(data.products);
-});
+app.get("/users", (req, res) => res.json(data.users));
+app.get("/products", (req, res) => res.json(data.products));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
